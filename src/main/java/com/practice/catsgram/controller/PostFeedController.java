d49fd776cf8e548b71b755247e5f48d6f56a9db6
= <=>
2024-08-21 18:19:29 +0500

    fix:

--- src/main/java/com/practice/catsgram/controller/PostFeedController.java
+++ src/main/java/com/practice/catsgram/controller/PostFeedController.java
@@ -1,7 +1,5 @@
 package com.practice.catsgram.controller;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.practice.catsgram.model.Post;
 import com.practice.catsgram.service.PostService;
 import lombok.Getter;
@@ -19,37 +17,25 @@ import java.util.List;
 public class PostFeedController {
 
     private final PostService postService;
-    private final ObjectMapper objectMapper = new ObjectMapper();
 
     @PostMapping("/feed/friends")
-    List<Post> getFriendsFeed(@RequestBody String params) {
-        FriendsParams friendsParams = parseParams(params);
-
-        if (friendsParams == null || friendsParams.getFriends().isEmpty()) {
+    List<Post> getFriendsFeed(@RequestBody FeedParams feedParams) {
+        if (feedParams == null || feedParams.getFriendsEmails().isEmpty()) {
             throw new IllegalArgumentException("Неверно заполнены параметры");
         }
 
         List<Post> result = new ArrayList<>();
-        for (String friend : friendsParams.friends) {
-            result.addAll(postService.findAllByUserEmail(friend, friendsParams.size, friendsParams.sort));
+        for (String friend : feedParams.friendsEmails) {
+            result.addAll(postService.findAllByUserEmail(friend, feedParams.size, feedParams.sort));
         }
         return result;
     }
 
-    private FriendsParams parseParams(String params) {
-        try {
-            String paramsFromString = objectMapper.readValue(params, String.class);
-            return objectMapper.readValue(paramsFromString, FriendsParams.class);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException("Невалидный формат json", e);
-        }
-    }
-
     @Setter
     @Getter
-    private static class FriendsParams {
+    private static class FeedParams {
         private String sort;
         private Integer size;
-        private List<String> friends;
+        private List<String> friendsEmails;
     }
 }
