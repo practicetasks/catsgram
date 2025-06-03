292b669ed6f7be07793026b150d5e0c257ff2230
practice_tasks <122010324+practicetasks@users.noreply.github.com>
2025-06-03 12:54:54 +0500

    Update User.java

--- src/main/java/com/practice/catsgram/model/User.java
+++ src/main/java/com/practice/catsgram/model/User.java
@@ -1,6 +1,7 @@
 package com.practice.catsgram.model;
 
 import java.time.LocalDate;
+import java.util.Objects;
 
 public class User {
     private String email;
@@ -35,5 +36,17 @@ public class User {
 
     public void setBirthdate(LocalDate birthdate) {
         this.birthdate = birthdate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(email, user.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(email);
     }
 }
